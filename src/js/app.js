b458f72b83ffcfbdbc57ee48516f60062ada839c
Ricardo Miguel R <ricardomr.23@hotmail.com>
2024-02-25 09:53:46 +0000

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,34 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name === null ? "Name" : variables.name} ${
+    variables.lastName === null ? "| Last Name" : variables.lastName
+  }</h1>
+          <h2>${variables.role === null ? "Role played" : variables.role}</h2>
+          <h3>${variables.city === null ? "City" : variables.city} ${
+    variables.country === null ? "| Country" : variables.country
+  }</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="${
+              variables.twitter === null
+                ? "https://twitter.com/"
+                : "https://twitter.com/" + variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="${
+              variables.github === null
+                ? "https://github.com/"
+                : "https://github.com/" + variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="${
+              variables.linkedin === null
+                ? "https://linkedin.com/"
+                : "https://linkedin.com/" + variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${
+              variables.instagram === null
+                ? "https://instagram.com/"
+                : "https://instagram.com/" + variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -54,9 +74,10 @@ window.onload = function() {
     // if includeCover is true the algorithm should show the cover image
     includeCover: true,
     // this is the image's url that will be used as a background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://w0.peakpx.com/wallpaper/172/859/HD-wallpaper-uefa-champions-league-background-estadio-de-futbol-papel-tapiz-deportivo-fondo-de-pantalla-futbol-soccer-pitch.jpg",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL: "https://avatars.githubusercontent.com/u/140925782?v=4",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
